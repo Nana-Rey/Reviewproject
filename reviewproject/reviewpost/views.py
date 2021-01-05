19b92d7f243186e86a5cf95a11ee3afd76f324a2
Nana-Rey <nanapen305@gmail.com>
2021-01-05 11:14:03 +0900

    Add reviewproject

--- reviewproject/reviewpost/views.py
+++ reviewproject/reviewpost/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def signupview(request):
     if request.method == 'POST':
         username_data=request.POST['username_data']
         password_data=request.POST['password_data']
-        user=User.objects.create_user(username_data,'',password_data)
+        try:
+            User.objects.create_user(username_data,'',password_data)
+        except IntegrityError:
+            return render(request,'signup.html',{'error':'このユーザーは既に登録されています。'})
     else:
         return render(request, 'signup.html',{})
     return render(request, 'signup.html',{})
