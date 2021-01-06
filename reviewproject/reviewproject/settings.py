3efe407736479967df526b123ebd1cc187cd1ead
Nana-Rey <nanapen305@gmail.com>
2021-01-06 17:48:20 +0900

    Add reviewproject

--- reviewproject/reviewproject/settings.py
+++ reviewproject/reviewproject/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfile/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/image/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
